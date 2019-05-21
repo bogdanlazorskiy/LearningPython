cfcceb12a1cf1bc7e59edfdd1ffc9c2796453d32
BogdanLazorskiy <lazorskiy99@gmail.com>
2019-05-21 18:27:28 +0300

    Update Lesson-3.py

--- Lesson-3.py
+++ Lesson-3.py
@@ -4,3 +4,13 @@ print('ab\tc')
 print('abc\t')
 letters = '1234567890'
 print(letters[4])
+soup='abcdefghjklmnopqrstuvwxyz'
+print(soup[2:10:2])
+print(len(soup))
+todos='god soup.yay book. loop give'
+print(todos.split('.'))
+print(todos.split())
+example=['Good','Yeti','Join']
+example_join=', '.join(example)
+print(example_join)
+print(example_join.startswith('Good'))
